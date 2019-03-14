09b44a5b729dd9c6a1661766120eed333cb1fed6
carlesalonso <carlos.martinez@mataro.epiaedu.cat>
2019-03-14 11:21:41 +0100

    separat en dos projectes

---
+++ SocketExempleClient/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketExempleClient
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var client = new Client();
+            client.Run();
+            Console.ReadKey();
+        }
+
+        class Client
+        {
+
+
+            public string ServerAddress { get; }
+
+            public int Port { get; }
+
+            public Client() => (ServerAddress, Port) = ("localhost", 23000);
+
+
+            public void Run()
+            {
+
+                    TcpClient client = new TcpClient(ServerAddress, Port);
+                try { 
+                    using (NetworkStream channel = client.GetStream())
+                    {
+                        BinaryReader reader = new BinaryReader(channel);
+                        BinaryWriter writer = new BinaryWriter(channel);
+                        string message = null;
+
+                        do
+                        {
+                            Console.Write("Enter message (:Q to exit): ");
+                            message = Console.ReadLine();
+
+                            writer.Write(message);
+                            writer.Flush();
+                            message = reader.ReadString();
+                            Console.WriteLine(message);
+                        } while (!message.Contains(":Q"));
+                    }
+
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("Socket error" + e.ToString());
+                }
+                finally
+                {
+                    client.Close();
+                    client.Dispose();
+                }
+            }
+        }
+    }
+}
+
+
+
+
+        
+
--- socketExample/Program.cs
+++ socketExample/Program.cs
@@ -1,103 +1,78 @@
 ﻿using System;
-using System.Collections.Generic; 
-using System.Linq; 
-using System.Text;  
-using System.Threading.Tasks;
-using System.Threading;
-using System.Net;  
-using System.Net.Sockets;
 using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace socketExample
 {
     class Program
     {
-        private static int _port = 5555;
+       
         static  void Main(string[] args)
         {
-            Server server = new Server(){port = _port};
-            Task.Run(() => server.Run());
-            Client client = new Client() { port = _port };
-            client.Run();
+            Server server = new Server();
+            server.Run();
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
 
         }
     }
 
-    class Server 
+    class Server
     {
-         public int port
+        public int Port { get; }
+
+        public Server() => Port = 23000;
+        public Server(int port)
         {
-            get;
-            set;
+            if (port > 1024 && port < 65536)
+                Port = port;
+            else
+                Port = 23000;
         }
+
 
         public void Run()
         {
-            try{
-                TcpListener listener = new TcpListener(IPAddress.Any, port);
+
+            var listener = new TcpListener(IPAddress.Any, Port);
+
+            try
+            {
                 listener.Start();
                 Console.WriteLine("Server start listening");
                 TcpClient clientConnected = listener.AcceptTcpClient();
                 Console.WriteLine("Client connected");
-                NetworkStream channel = clientConnected.GetStream();
-                BinaryReader reader = new BinaryReader(channel);
-                BinaryWriter writer = new BinaryWriter(channel);
-                string message = null;
-                do
+                using (NetworkStream channel = clientConnected.GetStream())
                 {
-                    message = reader.ReadString();
-                    writer.Write("Server >> you wrote " + message);
-                    writer.Flush();
-                } while (message != ":Q");
-                Console.WriteLine("Client desconnected");
-                channel.Close();
-                listener.Stop();
-
-            }
-            catch (SocketException e){
-                Console.WriteLine("Socket error" + e.ToString());
-            }
-        }
-    }
-
-
-    class Client
-    { 
-        public int port
-        {
-            get;
-            set;
-        }
-         public void Run(){
-            try {
-                TcpClient client = new TcpClient("localhost", port);
-                using (NetworkStream channel = client.GetStream()){
                     BinaryReader reader = new BinaryReader(channel);
                     BinaryWriter writer = new BinaryWriter(channel);
                     string message = null;
-                    Thread.Sleep(500);
-
                     do
                     {
-                        Console.Write("Enter message (:Q to exit): ");
-                        message = Console.ReadLine();
-
-                        writer.Write(message);
-                        writer.Flush();
                         message = reader.ReadString();
-                        Console.WriteLine(message);
-                    } while (!message.Contains(":Q"));
+                        writer.Write("Server >> you wrote " + message);
+                        writer.Flush();
+                    } while (message != ":Q");
+                    Console.WriteLine("Client desconnected");
                 }
-                client.Close();
-                client.Dispose();
             }
             catch (SocketException e)
             {
                 Console.WriteLine("Socket error" + e.ToString());
             }
 
+            finally
+            {
+
+                listener.Stop();
+            }
+
+
         }
+
+
+
     }
+
 }
